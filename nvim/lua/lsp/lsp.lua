0f7e0d9bce11e649091c4c6c3c9fbdd50ec20845
Furqan Tariq <furqant09@gmail.com>
2022-06-20 23:11:43 +0200

    LSP

--- nvim/lua/lsp/lsp.lua
+++ nvim/lua/lsp/lsp.lua
@@ -1,6 +1,16 @@
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
